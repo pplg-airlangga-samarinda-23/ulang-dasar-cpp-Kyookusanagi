6c8dc1b7738288749e0608a8b1db4b3417da30c0
Kyookusanagi <afifmp01@gmail.com>
2024-07-31 11:27:41 +0800

    selesai tugas

--- kode1.cpp
+++ kode1.cpp
@@ -1,7 +1,24 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-    cout << "Selamat Datang di SMK TI Airlangga" << endl;
-    return 0;
+void cetakData(string nama, string nisn, string jurusan, int tinggibadan)
+{
+cout << "Nama anda: " << nama << endl;
+cout << "Nisn anda: " << nisn << endl;
+cout << "Jurursan anda: " << jurusan << endl;
+cout << "Tinggi badan anda: " << tinggibadan << endl;
+
+
+
+}
+
+int main ()
+{
+    string nama = "Afif maulana putra";
+    string nisn = "00642821";
+    string jurusan = "PPLG";
+    int tinggibadan = 165 ;
+
+cetakData(nama, nisn, jurusan, tinggibadan);
+return 0;
 }
